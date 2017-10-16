d31bdc334b29f6a9d08761dcf8afb62d94da14fc
wangshijun2010 <wangshijun2010@gmail.com>
2017-10-16 08:02:44 +0800

    lesson 05: count events

---
+++ app/scripts/05-count-events.js
@@ -0,0 +1,8 @@
+for (let i = 0; i < 100; i++) {
+  const number = Math.number() * 100;
+  if (number > 50) {
+    console.count('number > 50');
+  } else {
+    console.count('number < 50');
+  }
+}
