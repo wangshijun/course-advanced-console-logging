d2c9163af907aa98ced4421c96aa906dc60b52b7
wangshijun2010 <wangshijun2010@gmail.com>
2017-10-16 08:00:32 +0800

    lesson 02: advanced console log arguments

---
+++ app/scripts/02-advanced-arguments.js
@@ -0,0 +1,8 @@
+// arbitary type of arguments
+console.log('hello', 'world', 123, { key: 'value' });
+
+// c-style string templates
+console.log('my name is %s, i am %d years old', 'wangshijun', 28);
+
+// apply css to log output
+console.log('my name is %cwangshijun', 'background:red;color:white;font-size:40px;');
